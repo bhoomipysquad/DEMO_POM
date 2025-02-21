8cf52ae4d6a37f39b1379665fb1973d963b46ade
Bhoomi Darji <bhoomi.pysquad@gmail.com>
2025-02-21 15:40:46 +0530

    21/2

--- Page/homepage.py
+++ Page/homepage.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium.webdriver.common.by import By
 
 
@@ -5,10 +7,13 @@ class HomePage:
     def __init__(self, driver):
         self.driver = driver
         self.inventory_items = (By.CLASS_NAME, "inventory_item")
-        self.logout_button = (By.ID, "logout_sidebar_link")
+        self.logout_button = (By.XPATH,'//*[@id="logout_sidebar_link"]')
+        self.click_menu = (By.XPATH,'//*[@id="react-burger-menu-btn"]')
 
     def get_inventory_items(self):
         return self.driver.find_elements(*self.inventory_items)
 
     def click_logout(self):
+        self.driver.find_element(*self.click_menu).click()
+        time.sleep(3)
         self.driver.find_element(*self.logout_button).click()
--- test/test_home.py
+++ test/test_home.py
@@ -1,8 +1,13 @@
+from Page.LoginPage import LoginPage
 from Page.homepage import HomePage
 
 
 def test_inventory_items(driver):
     # After login, we should be on the home page
+    login_page = LoginPage(driver)
+    login_page.enter_username(username = "standard_user")
+    login_page.enter_password(password = "secret_sauce")
+    login_page.click_login()
     home_page = HomePage(driver)
     inventory_items = home_page.get_inventory_items()
     assert len(inventory_items) > 0, "No inventory items found"
@@ -10,8 +15,14 @@ def test_inventory_items(driver):
 
 def test_logout(driver):
     # Logout from the home page
+
+    login_page = LoginPage(driver)
+    login_page.enter_username(username = "standard_user")
+    login_page.enter_password(password = "secret_sauce")
+    login_page.click_login()
     home_page = HomePage(driver)
     home_page.click_logout()
 
+
     # Check that the user is redirected to the login page
-    assert "login" in driver.current_url, "Logout failed"
+    assert  driver.current_url == "https://www.saucedemo.com/"
