800911280058855d1f33e0ccf84f2edf072a529d
Bhoomi Darji <bhoomi.pysquad@gmail.com>
2025-02-21 15:54:58 +0530

    21/2

--- test/test_home.py
+++ test/test_home.py
@@ -5,8 +5,8 @@ from Page.homepage import HomePage
 def test_inventory_items(driver):
     # After login, we should be on the home page
     login_page = LoginPage(driver)
-    login_page.enter_username(username = "standard_user")
-    login_page.enter_password(password = "secret_sauce")
+    login_page.enter_username("standard_user")
+    login_page.enter_password("secret_sauce")
     login_page.click_login()
     home_page = HomePage(driver)
     inventory_items = home_page.get_inventory_items()
@@ -17,8 +17,8 @@ def test_logout(driver):
     # Logout from the home page
 
     login_page = LoginPage(driver)
-    login_page.enter_username(username = "standard_user")
-    login_page.enter_password(password = "secret_sauce")
+    login_page.enter_username("standard_user")
+    login_page.enter_password("secret_sauce")
     login_page.click_login()
     home_page = HomePage(driver)
     home_page.click_logout()
