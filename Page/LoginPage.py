e7c8289bebe662df9cd2987e17bb768de0bb341f
Bhoomi Darji <bhoomi.pysquad@gmail.com>
2025-02-21 15:21:02 +0530

    21/2

--- Page/LoginPage.py
+++ Page/LoginPage.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
@@ -5,6 +7,7 @@ from selenium.webdriver.common.keys import Keys
 class LoginPage:
     def __init__(self, driver):
         self.driver = driver
+
         self.username_field = (By.ID, "user-name")
         self.password_field = (By.ID, "password")
         self.login_button = (By.ID, "login-button")
@@ -12,12 +15,15 @@ class LoginPage:
 
     def enter_username(self, username):
         self.driver.find_element(*self.username_field).send_keys(username)
+        time.sleep(2)
 
     def enter_password(self, password):
         self.driver.find_element(*self.password_field).send_keys(password)
+        time.sleep(2)
 
     def click_login(self):
         self.driver.find_element(*self.login_button).click()
+        time.sleep(3)
 
     def get_error_message(self):
         return self.driver.find_element(*self.error_message).text
